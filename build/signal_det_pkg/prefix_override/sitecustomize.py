abec171ca139167e8847347732d83e78b74aa2b3
Ivan Prieto <masterivi09@gmail.com>
2025-11-02 23:48:22 +0100

    Deteccion de señales y gitignores

--- build/signal_det_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/hugo/Documentos/RO/Practica1-RO/install/signal_det_pkg'
--- build/super_lane_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/hugo/Documentos/RO/Practica1-RO/install/super_lane_pkg'
